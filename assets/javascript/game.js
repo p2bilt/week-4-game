656302dbff90913941cf24f5e2a672eca677f76d
p2bilt <peteralan@gmail.com>
2017-12-19 22:36:24 -0500

    added jquery alerts, cleaned up code commenting.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,59 @@
+// BACKUP
+
 $(document).ready(function() {
 
     // variables to set the range of our random numbers.
+
     const goalMinNumber = 19;
     const goalMaxNumber = 120;
-    const gemMinNumber = 1;
-    const gemMaxNumber = 12;
 
     // array to hold our gem values
-    let randomGemNumberSet = new Array();
+
+    var randomGemNumberSet = new Array();
 
     // call a function and insert a random 'goal' number into variable
+
     var randomGoalNumber = randomNumberFromRange(goalMinNumber, goalMaxNumber);
 
     // function that picks a random number from a range
+
     function randomNumberFromRange(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     // function with a for loop which inserts four random numbers into an array
+
     function gemNumberMaker() {
 
-    for (let i = 0; i < 4; i++) {
-        var randomGemNumber = randomNumberFromRange(gemMinNumber, gemMaxNumber);
-        // insert numbers into array
-        randomGemNumberSet.push(randomGemNumber);
-    }
+        for (let i = 0; i < 4; i++) {
+            var randomGemNumber = randomNumberFromRange(1, 12);
+
+            // insert numbers into array
+
+            randomGemNumberSet.push(randomGemNumber);
+        }
     }
 
     // call that function to get gem numbers
+
     gemNumberMaker();
 
     // Just checking
+
     console.log(randomGoalNumber);
     console.log(randomGemNumberSet);
 
     // print our random goal number to page
+
     $("#number-goal").text(randomGoalNumber);
 
     // to iterate the collective total of our gem-clicks
+
     var counter = 0;
     $("#personal-num").text(counter);
 
-    // somewhat array to store individual image tags
+    // array to store individual image tags, which generate
+
     const gemPics = [
         "blu",
         "purp",
@@ -55,68 +67,72 @@ $(document).ready(function() {
     // Create multiple crystals each with their own unique number value, 
     // tucked inside a function for reset purposes.
     function generateCrystals() {
-    for (var i = 0; i < randomGemNumberSet.length; i++) {
+        for (let i = 0; i < randomGemNumberSet.length; i++) {
 
-        // For each iteration, we will create an imageCrystal
-        var imageCrystal = $("<img>");
+            // For each iteration, we will create an imageCrystal
+            var imageCrystal = $("<img>");
 
-        // First each crystal will be given the class ".crystal-image".
-        // This will allow the CSS to take effect.
-        imageCrystal.addClass("crystal-image");
+            // each crystal be given the class ".crystal-image". (onclick targets this)
+            imageCrystal.addClass("crystal-image");
 
-        // a random number from zero-six to shuffle our gem images
-        var x = randomNumberFromRange(0, 6);
+            // a random number from zero-six to shuffle our gem images
+            var x = randomNumberFromRange(0, 6);
 
-        // Each imageCrystal will be given a src link to the crystal image
-        imageCrystal.attr("src", "assets/images/gem_" + gemPics[x] + ".png");
+            // a random src link to the crystal image
+            imageCrystal.attr("src", "assets/images/gem_" + gemPics[x] + ".png");
 
-        // Each imageCrystal will be given a data attribute called data-crystalValue.
-        // This data attribute will be set equal to the array value.
-        imageCrystal.attr("data-crystalvalue", randomGemNumberSet[i]);
+            // Each imageCrystal will be given a data attribute called data-crystalValue.
+            // This data attribute will be set equal to the array value.
+            imageCrystal.attr("data-crystalvalue", randomGemNumberSet[i]);
 
-        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-        $("#crystals").append(imageCrystal);
-    }
+            // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+            $("#crystals").append(imageCrystal);
+        }
     }
 
     generateCrystals();
 
     // Scorekeeping
-    let wins = 0;
-    let losses = 0;
+    var wins = 0;
+    var losses = 0;
 
 
-    // capture click event 
+    // capture click event, and specifically target the document so the buttons work! Thanks Noah!
     $(document).on("click", ".crystal-image", function() {
 
-        // Determining the crystal's value requires us to extract the value from the data attribute.
-        // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-        // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-        // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+        // grab value from clicked crystal, convert string to integer 
 
         var crystalValue = ($(this).attr("data-crystalvalue"));
         crystalValue = parseInt(crystalValue);
 
-        // We then add the crystalValue to the user's "counter" which is a global variable.
-        // Every click, from every crystal adds to the global counter.
+        // Add the clicked-crystal's value to the global counter
+
         counter += crystalValue;
 
-        // All of the same game win-lose logic applies. So the rest remains unchanged.
+        // Update the dom with the newly calculated total of all clicked crystals
 
         $("#personal-num").text(counter);
 
-        // if/else to test if our personal score equals the goal number
+        // test if our personal score equals - or exceeds! -  the goal number
 
         if (counter === randomGoalNumber) {
-            alert("Alignment achieved!\n\nTry again to increase your astral harmonic.\n\nHere's some new crystals for you.");
+            $.alert({
+                title: 'Congratulations!',
+                content: "<br><strong>Alignment achieved!</strong><br><br>Try again to increase your astral harmonic.<br><br>Here's some new crystals for you.",
+                type: 'green',
+                typeAnimated: true,
+            });
             wins++;
             $("#winner").text(wins);
             gameReset();
-            
-        } 
 
-        else if (counter >= randomGoalNumber) {
-            alert("What's the frequency, Kenneth??\n\nWell, you've lost this time–-But why not try again?\n\nHere's some new crystals for you.");
+        } else if (counter >= randomGoalNumber) {
+            $.alert({
+                title: 'Uhhh...',
+                content: "<br>What's the frequency, Kenneth??<br><br><strong>Well, you've lost this time–-<br>But why not try again?</strong><br><br>Here's some new crystals for you.",
+                type: 'red',
+                typeAnimated: true,
+            });
             losses++;
             $("#loser").text(losses);
             gameReset();
@@ -125,8 +141,9 @@ $(document).ready(function() {
 
     });
 
-        // function to reset the game
-    function gameReset() {	
+    // function to reset the game
+
+    function gameReset() {
         counter = 0;
         $("#personal-num").text(counter);
         randomGoalNumber = randomNumberFromRange(goalMinNumber, goalMaxNumber);
